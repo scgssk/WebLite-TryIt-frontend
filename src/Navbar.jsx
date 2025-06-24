9565250b99d5af7f708161f95a85064217d2a822
Sree Soorya Kumar S C G <152139536+scgssk@users.noreply.github.com>
2025-06-24 18:12:17 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import './Navbar.css';
 import logo from './assets/weblite-logo.png'; // adjust path if needed
 
 function Navbar({ onRunClick, darkMode, toggleTheme }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={`navbar ${darkMode ? 'dark' : 'light'}`}>
       <div className="nav-left">
         <img src={logo} alt="WebLite Logo" className="logo" />
 
-        <a href="https://github.com/scgssk/WebLite-DSL/blob/Master/Readme.md" target='_blank'>Docs</a>
-        <a href="https://github.com/scgssk/WebLite-DSL/blob/Master/Examples.md" target='_blank'>Examples</a>
-        <a href="https://github.com/scgssk/WebLite-DSL/" target='_blank'>GitHub</a>
+        <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </button>
+
+        <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <a href="https://github.com/scgssk/WebLite-DSL/blob/Master/Readme.md" target='_blank'>Docs</a>
+          <a href="https://github.com/scgssk/WebLite-DSL/blob/Master/Examples.md" target='_blank'>Examples</a>
+          <a href="https://github.com/scgssk/WebLite-DSL/" target='_blank'>GitHub</a>
+        </div>
       </div>
+
       <div className="nav-right">
         <button className="run-btn" onClick={onRunClick}>▶ Run</button>
         <label className="theme-switch">
